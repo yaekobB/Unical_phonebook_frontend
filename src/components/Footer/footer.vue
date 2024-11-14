5b309bc43f9544809c3d004ffdfeece2ba63bd09
Robel_GH <robelgh103@gmail.com>
2024-11-14 17:23:40 +0100

    UP-64 feat: Adding Footer Component

---
+++ src/components/Footer/footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-footer
+    class="text-center d-flex flex-column"
+    color="red-darken-4"
+  >
+    <div>
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        :icon="icon"
+        class="mx-4"
+        variant="text"
+      ></v-btn>
+    </div>
+
+    <div class="pt-0">
+      Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+    </div>
+
+    <v-divider></v-divider>
+
+    <div>
+      {{ new Date().getFullYear() }} — <strong>© Unical Phonebook</strong>
+    </div>
+  </v-footer>
+</template>
