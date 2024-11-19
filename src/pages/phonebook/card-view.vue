340f57082926e9020b71b74750cfaebd9e9aaaa8
Robel_GH <robelgh103@gmail.com>
2024-11-19 05:12:14 +0100

    [UP-84, UP-82, UP-86, UP-79, UP-64] feat: pagination, search, phonebook error handling, filter, public phonebook page functionalities completed

--- src/pages/phonebook/card-view.vue
+++ src/pages/phonebook/card-view.vue
@@ -1,23 +1,27 @@
 <template>
   <v-container>
-    <v-row justify="center">
+    <v-row >
       <!-- <v-col cols="6"></v-col> -->
-      <v-col cols="12"  md="6" lg="4" xl="2"  v-for="contact in contacts"
-          :key="contact.id"
-          class="mb-4">
+      <v-col cols="12" sm="6" md="4" lg="3" xl="2"  v-for="contact in contacts"
+          :key="contact.userId"
+          class="mb-4 ">
         <v-card
-         
           color="error"
           variant="outlined"
-        
+          min-height="400"
+          max-height="400"
+          min-width="290"
           rounded="xl"
+         
+          fixed
+         
         >
-          <!-- Header Section -->
+            <!-- Header Section -->
         
           <v-sheet color="#8a3c36">
             <v-card-item>
               <template v-slot:prepend>
-                <v-card-title class="text-h6">
+                <v-card-title class="text-body-2">
                       <!-- <v-avatar color="info">
                         <span class="text-h5">RG</span>
                     </v-avatar> -->
@@ -42,7 +46,10 @@
           
 
           <!-- Contact Details Section -->
-          <v-card class="ma-2"  rounded="xl" variant="outlined"  color="error">
+          <v-card rounded="xl" variant="outlined"  
+          color="error" min-height="270"
+          max-height="270"
+           class="scrollable-card ma-3">
              <!-- Contact Information Section -->
             <v-card-item>
               <v-card-title class="text-body-2 d-flex align-center">
@@ -54,8 +61,9 @@
               </v-card-title>
               <div class="py-2">
                 <div class="font-weight-light text-medium-emphasis">
-                 <!-- {{contact.department}} -->
-                 (Macro) Settore Servizi didattica e studenti DESF - Dipartimento di Economia, Statistica e Finanza
+                 <router-link to="">
+                  {{contact.department}}</router-link> 
+                 <!-- (Macro) Settore Servizi didattica e studenti DESF - Dipartimento di Economia, Statistica e Finanza -->
                  
                   <br/>
                 {{contact.userType}}
@@ -70,53 +78,49 @@
               <v-card-title class="text-body-2 d-flex align-center">
                 <v-icon  icon="mdi-map-marker" start></v-icon>
                 <span class="text-medium-emphasis font-weight-bold">
-                  Address
+                  Office
                 </span>
                 <v-spacer></v-spacer>
               </v-card-title>
               <div class="py-2">
                 
                 <div class="font-weight-light text-medium-emphasis">
-                 Cubo 30B - MT11
-                 
+                {{contact.address}}
+               
                 
                 </div>
               </div>
             </v-card-item>
-            <v-divider></v-divider>
-
-            <!-- Office Hours Section -->
-            <!-- <v-card-item>
-              <v-card-title class="text-body-2 d-flex align-center">
-                <v-icon color="#949cf7" icon="mdi-clock" start></v-icon>
-                <span class="text-medium-emphasis font-weight-bold">
-                  Office Hours
-                </span>
-                <v-spacer></v-spacer>
-              </v-card-title>
-              <div class="py-2">
-                <div class="">Monday - Friday: 9:00 AM - 5:00 PM</div>
-                <div class="">Saturday: 10:00 AM - 2:00 PM</div>
-              </div>
-            </v-card-item> -->
-            <v-divider></v-divider>
-
            
           </v-card>
 
         
 
           <!-- Actions Section -->
-          <div class="pa-2 d-flex align-center" rounded="xl">
+          <div class="d-flex align-center" rounded="xl">
           
             <v-spacer></v-spacer>
               <!-- Social Media Section -->
-          <div class="pa-1 d-flex align-center">
-            <v-icon color="#e1306c" icon="mdi-phone" size="24" class="me-3"></v-icon>
-            <v-icon color="#3b5998" icon="mdi-email" size="24" class="me-3"></v-icon>
-             <v-icon color="#0077b5" icon="mdi-web" size="24" class="me-3"></v-icon>
-            <v-icon color="#1da1f2" icon="mdi-x" size="24" class="me-3"></v-icon>
-            <v-icon color="#0077b5" icon="mdi-linkedin" size="24" class="me-3"></v-icon>
+          <div class=" d-flex align-center">
+             <v-btn icon class="white--text " href="" target="_blank" v-tooltip="contact.phoneNumber">
+              <v-icon color="#e1306c" icon="mdi-phone" size="24" class="ma-3"></v-icon>
+            </v-btn>
+            <v-btn icon class="white--text" :href="contact.email" target="_blank">
+              <v-icon color="#3b5998" icon="mdi-email" size="24" class="ma-3"></v-icon>
+            </v-btn>
+            <v-btn icon class="white--text" :href="contact.websiteLink" target="_blank">
+              <v-icon color="#0077b5" icon="mdi-web" size="24" class="ma-3"></v-icon>
+            </v-btn>
+           
+            <v-btn icon class="white--text" :href="contact.linkedIn" target="_blank">
+             
+                 <v-icon color="#0077b5" icon="mdi-linkedin" size="24" class="ma-3"></v-icon>
+            </v-btn>
+           
+           
+             
+          
+          
            
            
           </div>
@@ -128,16 +132,39 @@
 </template>
 
 <script>
+import  dummyData  from './dummydata';
+
 export default {
   name: "ContactDetailsCard",
   props: {
-  contacts: {
-    type: Array,
-    default: () => []
-  }
-},
+    contacts: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed:{
+    // getContacts(){
+    //   let mappedContacts = this.contacts.map(contact => contact.raw)
+    //   console.log(mappedContacts)
+    //   return mappedContacts
+    // }
+
+  },
   data(){
     return{
+    // contacts: dummyData
+      //[
+      //   {firstName:"Robel",middleName:"GH", lastName:"Brhane",email:"robel@unical.it", phoneNumber:"+393511141072", department:"(Macro) Settore Servizi didattica e studenti DESF - Dipartimento di Economia, Statistica e Finanza  DESF - Dipartimento di Economia, Statistica e Finanza "},
+      //   {firstName:"Robel",middleName:"GH", lastName:"Brhane",email:"robel@unical.it", phoneNumber:"+393511141072", department:"Area Servizi TA – DIMES: Dipartimento Ingegneria Informatica, Modellistica, Elettronica e Sistemistica"},
+      //   {firstName:"Robel",middleName:"GH", lastName:"Brhane",email:"robel@unical.it", phoneNumber:"+393511141072", department:"Demacs"},
+      //   {firstName:"Robel",middleName:"GH", lastName:"Brhane",email:"robel@unical.it", phoneNumber:"+393511141072", department:"Demacs"},
+      //   {firstName:"Robel",middleName:"GH", lastName:"Brhane",email:"robel@unical.it", phoneNumber:"+393511141072", department:"Demacs"},
+      //   {firstName:"Robel",middleName:"GH", lastName:"Brhane",email:"robel@unical.it", phoneNumber:"+393511141072", department:"Demacs"},
+      //   {firstName:"Robel",middleName:"GH", lastName:"Brhane",email:"robel@unical.it", phoneNumber:"+393511141072", department:"Demacs"},
+      //   {firstName:"Robel",middleName:"GH", lastName:"Brhane",email:"robel@unical.it", phoneNumber:"+393511141072", department:"Demacs"},
+      //   {firstName:"Robel",middleName:"GH", lastName:"Brhane",email:"robel@unical.it", phoneNumber:"+393511141072", department:"Demacs"},
+
+      // ]
         
     }
 
@@ -149,5 +176,8 @@ export default {
 .v-card-title h3 {
   margin: 0;
   font-weight: bold;
+}
+ .scrollable-card {
+  overflow-y: auto;
 }
 </style>
--- src/pages/phonebook/phonebook.vue
+++ src/pages/phonebook/phonebook.vue
@@ -3,71 +3,84 @@
         <Toolbar/>
         
             <v-card class="ma-10">
-             <v-data-iterator
+             <!-- <v-data-iterator
                 :items="contacts"
-                :items-per-page="6"
+                :items-per-page="pageLimit"
                 :search="search"
-                
                 >
-                <template v-slot:header>
-                    <v-toolbar class="pa-4">
+                <template v-slot:header> -->
+                   <v-toolbar class="pa-4">
                     <v-text-field
                         v-model="search"
                         density="compact"
                         placeholder="Search Surname"
-                        prepend-inner-icon="mdi-magnify"
+                        append-inner-icon="mdi-magnify"
                         style="max-width: 300px;"
+                        @click:append-inner="searchFilter(search)"
                         variant="outlined"
-                        clearable
+                       
                         hide-details
                         rounded
                         class="ma-2"
                     ></v-text-field>
                      <v-autocomplete
                         label="Department"
-                        style="max-width: 325px;"
-                        multiple
+                        chips                      
                         variant="outlined"
                         density="compact"
-                         clearable
+                        :items="departments"
                         hide-details
                         rounded
                         class="ma-2"
+                        v-model="selectedDepartments"
                     ></v-autocomplete>
                     <v-autocomplete
                         label="Role"
-                        style="max-width: 325px;"
-                        multiple
+                         chips
                         variant="outlined"
                         density="compact"
-                         clearable
+                        :items="roles"
                         hide-details
                         rounded
                         class="ma-2"
+                        v-model="selectedRoles"
+                       
                     ></v-autocomplete>
+                 
                     <v-spacer></v-spacer>
                      <v-btn-toggle
                         rounded="xl"
                         divided
                         >
-                        <v-btn icon @click="toggleView('grid')">
+                        <v-btn icon @click="toggleView('card')">
                             <v-icon>mdi-view-grid</v-icon>
                         </v-btn>
-                        <v-btn icon @click="toggleView('list')">
+                        <v-btn icon @click="toggleView('table')">
                             <v-icon>mdi-view-list</v-icon>
                         </v-btn>
                         
                     </v-btn-toggle>
                     </v-toolbar>
-                </template>
+                <!-- </template>
 
-                <template v-slot:default="{ items }">
-                    <CardView :contacts="contacts"/>
-                    <!-- <TableView :contacts="contacts"/> -->
-                </template>
+                <template v-slot:default="{ items }"> -->
+                
+                    <CardView :contacts="contacts" v-if="viewStat == 'card'"/>
+                    <TableView :contacts="contacts" v-if="viewStat == 'table'"/>
+                    <v-pagination
+                    v-model="currentPage"
+                    :length="totalPage"
+                    class="my-4"
+                    :total-visible="3"
+                    next-icon="mdi-menu-right"
+                    prev-icon="mdi-menu-left"
+                    @click="controlPagination"
+                    />
+                 
+                <!-- </template> -->
 
-                <template v-slot:footer="{ page, pageCount, prevPage, nextPage }">
-                    <div class="d-flex align-center justify-center pa-4">
+                <!-- <template v-slot:footer="{ page, pageCount, prevPage, nextPage }"> -->
+                    <!-- <div class="d-flex align-center justify-center pa-4">
                     <v-btn
                         :disabled="page === 1"
                         density="comfortable"
@@ -89,9 +102,10 @@
                         rounded
                         @click="nextPage"
                     ></v-btn>
-                    </div>
-                </template>
-                </v-data-iterator>
+                    </div> -->
+                <!-- </template> -->
+                <!-- </v-data-iterator> -->
+                
             </v-card>
 
       
@@ -104,41 +118,109 @@
 import Toolbar from '@/components/toolbar/toolbar.vue'
 import Footer from '@/components/Footer/footer.vue'
 import CardView from '@/pages/phonebook/card-view.vue'
+import TableView from '@/pages/phonebook/table-view.vue'
 import { useUserStore } from '@/pages/user-account/store';
+// import roles from '@/services/roles'
+import departments from '@/services/departments'
 export default{
     name:'Phonebook',
     components:{
         Toolbar,
         Footer,
-        CardView
+        CardView,
+        TableView
     },
     data(){
         return {
-    
-            contacts:[]
+            userStore: useUserStore(),
+            roles: ["Student", "Faculty","Administrative"],
+            departments: departments,
+            contacts:[],
+            viewStat: 'card',
+            selectedDepartments:null,
+            selectedRoles:null,
+            pageLimit: 10,
+            search: '',
+            totalPage:0,
+            currentPage:1,
+           
+
         }
     },
-    async created() {
-    // const tableStore = useTableStore();
-     const userStore = useUserStore();
-    // const navSideStore = useSideNavStore();
-    // const columns = toRaw(userStore.tableColumns);
-    // navSideStore.setNavigations(userStore.navItems)
-    // tableStore.setColumns(userStore.tableColumns);
-    // tableStore.setFormFields(userStore.formFields);
-    // tableStore.setDialogTitle(userStore.dialogTitle);
-    // tableStore.setIcon(userStore.icon)
-    await userStore.getUsers();
-    this.contacts = userStore.users
-    console.log(userStore.users)
-    
-    // console.log(columns)
-    // Optionally, initialize with some items
-    // tableStore.initializeItems(userStore.users);
-    // tableStore.initializeItems([
-    //   {id:"1", firstName:"Robel", middleName:"Gebrehiwot", lastName:"Brhane", userRole:"Admin",email:"robel@unical.it",phoneNumber:"+393511141072"}
-    // ])
+    // computed:{
+    //     getTotalPage (){
+    //         return this.userStore.users.length / this.pageLimit
+    //     }
+
+    // },
+    watch: {
+    // Fetch data whenever the current page changes
+    currentPage() {
+      this.controlPagination('');
+    },
+    selectedDepartments(){
+        this.controlPagination(this.selectedDepartments);
+    },
+    selectedRoles(){
+        this.controlPagination(this.selectedRoles)
+    }
+
+    // search(){
+    //  this.controlPagination()
+    //  console.log(this.search)
+    // }
   },
+    methods:{
+        toggleView(view){
+            this.viewStat = view
+        },
+       async controlPagination(searchKey){
+        // console.log("Control pagination called")
+           // the fourth parameter is isPublic value which is true for public request
+             await this.userStore.getUsers(this.pageLimit, this.currentPage, searchKey,true);
+             this.contacts = this.userStore.users
+             if(this.contacts.length> 0){
+                 this.totalPage = Math.ceil(this.contacts[0].totalUsers / this.pageLimit)
+             }
+            
+             
+    
+        },
+        searchFilter(searchKey){
+            this.pageLimit = 25
+            this.currentPage = 1
+            this.controlPagination(searchKey)
+        }
+
+    },
+    async mounted() {
+        this.controlPagination()
+            // await this.userStore.getUsers('','','');
+            // this.totalPage = Math.ceil( this.userStore.users.length / this.pageLimit)
+     
+    // this.totalPage = this.userStore.users.length / this.pageLimit  // Initial data load
+  },
+//     async created() {
+//     // const tableStore = useTableStore();
+//      const userStore = useUserStore();
+//     // const navSideStore = useSideNavStore();
+//     // const columns = toRaw(userStore.tableColumns);
+//     // navSideStore.setNavigations(userStore.navItems)
+//     // tableStore.setColumns(userStore.tableColumns);
+//     // tableStore.setFormFields(userStore.formFields);
+//     // tableStore.setDialogTitle(userStore.dialogTitle);
+//     // tableStore.setIcon(userStore.icon)
+//     await userStore.getUsers(this.pageLimit, this.currentPage, this.search);
+//     this.contacts = userStore.users
+//     console.log(userStore.users)
+    
+//     // console.log(columns)
+//     // Optionally, initialize with some items
+//     // tableStore.initializeItems(userStore.users);
+//     // tableStore.initializeItems([
+//     //   {id:"1", firstName:"Robel", middleName:"Gebrehiwot", lastName:"Brhane", userRole:"Admin",email:"robel@unical.it",phoneNumber:"+393511141072"}
+//     // ])
+//   },
 
     
 }
