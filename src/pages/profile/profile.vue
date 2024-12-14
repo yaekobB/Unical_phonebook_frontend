3fb54f84d75ce7288db74d4648a9a89b553f433a
Yaekob Beyene <yaeko2024@gmail.com>
2024-12-14 01:50:21 +0100

    Change Password added

--- src/pages/profile/profile.vue
+++ src/pages/profile/profile.vue
@@ -63,26 +63,129 @@
               Edit Profile
             </v-btn>
           </v-card-actions>
+          <v-card-actions>
+            <v-btn color="red-darken-4" block  @click="toggleDialog">
+              <v-icon left>mdi-key</v-icon>
+              Change Password
+            </v-btn>
+          </v-card-actions>
         </v-card>
       </v-col>
 
       <!-- Tabs Column -->
-      <v-col cols="12" md="8">
+      <v-col  cols="12" md="8">
        <detail-profile :details="userDetail" />
       </v-col>
     </v-row>
+    <v-dialog v-model="dialog" max-width = "700px" persistent>
+      <v-card  class="elevation-12 forgot-password-card fixed" max-width="500" >
+        <v-card-title class="mb-3">
+         <router-link >
+           <v-icon color="primary" class="mr-2" large>
+            <v-img
+            cover
+            src="https://cdn.jsdelivr.net/gh/UniversitaDellaCalabria/unicms-template-unical@1.7.1/src/unicms_template_unical/static/images/addressbook.svg"></v-img>
+          </v-icon>
+         </router-link>
+          
+         Unical Phonebook: Change Password
+        </v-card-title>
+        <v-card-text class=" mr-8 ml-8 pa-2" variant="outlined" color="red-darken-4">
+          <v-form v-model="valid" ref="form">
+            <v-text-field 
+              v-model="oldPassword"
+              prepend-inner-icon="mdi-lock"
+              density = "comfortable"
+              label="Old Password"
+              :type="showOldPassword ? 'text' : 'password'"
+              :rules="[passowrdRules.password, passowrdRules.required]"
+               :append-inner-icon="showOldPassword ? 'mdi-eye-off' : 'mdi-eye'"
+              required
+              variant ="outlined"
+              @click:append-inner="toggleOldPasswordVisibility"
+            ></v-text-field>
+          
+            <v-text-field 
+              v-model="newPassword"
+              prepend-inner-icon="mdi-lock"
+              density = "comfortable"
+              label="New Password"
+              :type="showNewPassword ? 'text' : 'password'"
+              :rules="[passowrdRules.password, passowrdRules.required]"
+               :append-inner-icon="showNewPassword ? 'mdi-eye-off' : 'mdi-eye'"
+              required
+              variant ="outlined"
+              @click:append-inner="toggleNewPasswordVisibility"
+            ></v-text-field>
+
+            <v-text-field 
+              v-model="confirmPassword"
+              prepend-inner-icon="mdi-lock"
+              density = "comfortable"
+              label="Confirm Password"
+              :type="showConfirmPassword ? 'text' : 'password'"
+              :rules="[passowrdRules.confirmPassword, passowrdRules.required]"
+              :append-inner-icon="showConfirmPassword ? 'mdi-eye-off' : 'mdi-eye'"
+              required
+              @click:append-inner="toggleConfirmPasswordVisibility"
+              variant ="outlined"
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+            <v-row>
+              <v-col class="d-flex justify">
+                <v-btn
+                  color="error"
+                  @click="cancel"
+                  variant ="outlined"
+                >
+                 Cancel
+               </v-btn>
+              </v-col> 
+              <v-col class="d-flex justify-end">
+                <v-btn
+                  :disabled="!valid"
+                  @click="changePassword"
+                  variant="outlined"
+                >
+                 Change
+               </v-btn>
+              </v-col>                   
+           </v-row>
+      </v-card>
+    </v-dialog>
+   <Snackbar/>
   </v-container>
 </template>
 
 <script>
 import detailProfile from './detail-profile/detail-profile.vue';
-export default {
-  components: { detailProfile },
+import Snackbar from '@/components/snackbar/Snackbar.vue'; 
+import { useForgotPasswordStore } from '../../components/forgotPassword/store';
+ export default {
+  components: { detailProfile, Snackbar},
   name: "UserProfile",
   data() {
     return {
+      email: '',
+        password: '',
+        oldPassword:'',
+        newPassword:'',
+        confirmPassword:'',
+        showOldPassword: false,
+        showNewPassword: false,
+        showConfirmPassword: false,
+        valid: false,
+
+        passowrdRules:{
+          required: (v) => !!v || 'password is Required.',
+          password: (v) => v.length >= 6 && v != '' || "Password must be at least 6 characters",
+          confirmPassword: (v) => v.length >= 6 && v != '' && v === this.newPassword || "Passwords must match",
+        },
       
-      userDetail:{}
+        userDetail:{},
+        dialog:false,
+        messages:'',
     };
   },
   computed: {
@@ -94,10 +197,51 @@ export default {
   methods:{
      getUserDetail(){
       this.userDetail = JSON.parse(localStorage.getItem('userInformation'))
-     this.userDetail.fullName= this.userDetail.firstName +' '+ this.userDetail.middleName +' '+ this.userDetail.lastName
-      
-      
-    }
+     this.userDetail.fullName= this.userDetail.firstName +' '+ this.userDetail.middleName +' '+ this.userDetail.lastName     
+    },
+    async changePassword(){
+        //try{
+          
+          // this.userDetail  = JSON.parse(localStorage.getItem('userInformation'));
+          console.log("User Information: ", this.userDetail);
+          console.log("CP Sending request with: ", {
+            email: this.userDetail.email,
+            newPassword: this.newPassword,
+            oldPassword:this.oldPassword,
+          });
+          
+          const useChangePassword = useForgotPasswordStore();
+          this.email = this.userDetail.email;
+
+          let credentials ={
+            email: this.email,
+            newPassword: this.newPassword,
+            oldPassword:this.oldPassword,
+          }
+          useChangePassword.changePassword(credentials);
+          console.log("isPasswordChanged: ", useChangePassword.isPasswordChanged);
+
+          if(useChangePassword.isPasswordChanged){
+             this.dialog = !this.dialog;
+             useChangePassword.isPasswordChanged = false;
+          }
+    },    
+        toggleOldPasswordVisibility(){
+          this.showOldPassword =!this.showOldPassword;
+        },
+        toggleNewPasswordVisibility(){
+          this.showNewPassword =!this.showNewPassword;
+        },
+        toggleConfirmPasswordVisibility(){
+          this.showConfirmPassword =!this.showConfirmPassword;
+        },
+      toggleDialog(){
+        this.dialog = !this.dialog;
+      },
+      cancel(){
+        this.dialog = !this.dialog;
+
+      },
   },
   created(){
     this.getUserDetail()
@@ -108,5 +252,10 @@ export default {
 <style scoped>
 .text-center {
   text-align: center;
+}
+.forgot-password-card{
+  backdrop-filter: blur(6px);
+  padding: 24px;
+  border-radius: 12px;
 }
 </style>
